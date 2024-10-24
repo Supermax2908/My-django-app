8e13937c103ab7378b6eecfb7769351d9ece357f
maks byzovskiy <byzovskiy290808@gmail.com>
2024-10-24 19:25:15 +0300

    add comment for lessons in admin

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import LessonTag, LessonCategory, Lesson
+from .models import LessonTag, LessonCategory, Lesson, LessonComment
 # Register your models here.
 
 @admin.register(LessonTag)
@@ -17,9 +17,15 @@ class LessonCategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
     
 @admin.register(Lesson)
-class LessonTagAdmin(admin.ModelAdmin):
+class LessonAdmin(admin.ModelAdmin):
     list_filter = ('uiid', 'tag', 'category', 'author', 'user', 'topic', 'created_at')
     ordering = ('topic',)
     fields = ('uiid', 'tag', 'category', 'author', 'user', 'topic', 'description', 'cash', 'payment')
     readonly_fields = ('uiid', 'created_at')
     
+@admin.register(LessonComment)
+class LessonCommentAdmin(admin.ModelAdmin):
+    list_filter = ('id', 'user', 'lesson', 'comment', 'created_at')
+    ordering = ('comment',)
+    fields = ('id', 'user', 'lesson', 'comment', 'created_at')
+    readonly_fields = ('id', 'created_at')
